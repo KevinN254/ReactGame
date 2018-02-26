aa239b6fd44b9081396f6f57bb1384dd929c6943
kevin-ngunjiri <ngunjiri.kevin@yahoo.com>
2018-02-26 08:02:19 -0500

    Added square component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,3 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+class square extends React.Component{
+    render(){
+        return(
+            <button class name = "square">
+                {this.props.value}
+            </button>
+        );
+    }
+}
+
+class board extends React.Component{
+    renderSquare(i){
+        return <square/>;
+    }
+}
